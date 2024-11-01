66622a5b351d579637546341515bd4786336a2fe
Rustem <slehder5657@gmail.com>
2024-11-01 17:31:14 +0500

    debbug menu

--- src/layout/header/headerMenu/Menu.tsx
+++ src/layout/header/headerMenu/Menu.tsx
@@ -1,9 +1,14 @@
-import { FC } from "react";
+import { FC, useEffect, useState } from "react";
 import { S } from "../Header.styles";
 
 interface MenuProps {}
 
-const MenuItems = [
+interface MenuItem {
+  name: string;
+  link: string;
+}
+
+const menuItems: MenuItem[] = [
   { name: "Home", link: "home" },
   { name: "About", link: "about" },
   { name: "Works", link: "works" },
@@ -11,22 +16,45 @@ const MenuItems = [
 ];
 
 const Menu: FC<MenuProps> = () => {
-  const items = MenuItems.map((i) => {
-    return (
-      <S.MenuItem>
-        <S.MenuItemLink
-          to={i.link}
-          smooth={true}
-          duration={1000}
-          activeClass="active"
-          spy
-          offset={100}
-        >
-          {i.name}
-        </S.MenuItemLink>
-      </S.MenuItem>
-    );
-  });
+  const [activeSection, setActiveSection] = useState<string>("");
+
+  useEffect(() => {
+    const observerOptions = {
+      root: null,
+      rootMargin: `-50% 0px`,
+      threshold: 0,
+    };
+
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActiveSection(entry.target.id);
+        }
+      });
+    }, observerOptions);
+
+    menuItems.forEach((item) => {
+      const section = document.getElementById(item.link);
+      if (section) observer.observe(section);
+    });
+
+    return () => observer.disconnect();
+  }, []);
+
+  const items = menuItems.map((item, index) => (
+    <S.MenuItem key={index}>
+      <S.MenuItemLink
+        to={item.link}
+        smooth={true}
+        duration={1000}
+        activeClass="active"
+        spy
+        className={activeSection === item.link ? "active" : ""}
+      >
+        {item.name}
+      </S.MenuItemLink>
+    </S.MenuItem>
+  ));
 
   return (
     <S.Nav>
