a139a3ffd6e3c71b8cb8ccc6fd5fed463c59f033
Rustem <slehder5657@gmail.com>
2024-11-04 23:04:12 +0500

    redesign contact section and delete form

--- src/layout/sections/contacts/Contacts.styles.ts
+++ src/layout/sections/contacts/Contacts.styles.ts
@@ -6,68 +6,27 @@ const Contacts = styled.section`
   font-size: 20px;
 `;
 
-const MessageBlock = styled.div`
-  max-width: 700px;
-  width: 100%;
-`;
-
-const Form = styled.form`
-  display: flex;
-  flex-direction: column;
-  gap: 20px;
-  padding: 15px;
-
-  background-color: ${theme.colors.secondaryBg};
-
-  textarea {
-    height: 18px;
-    font-family: "Roboto";
-    resize: none;
-    line-height: 1;
-  }
-
-  ${StyledBtn} {
-    cursor: pointer;
-    text-align: end;
-
-    border: none;
-    border-top: 1px solid ${theme.colors.primaryBg};
-    color: ${theme.colors.font2};
-    background-color: rgba(0, 0, 0, 0);
-  }
-`;
-
-const Field = styled.input`
-  border: none;
-  border-bottom: 1px solid ${theme.colors.font2};
-  background-color: rgba(0, 0, 0, 0);
-
-  color: ${theme.colors.font2};
-
-  &::placeholder {
-    color: ${theme.colors.font2};
-
-    font-size: 15px;
-  }
-`;
-
 const PhoneNumber = styled.div`
   font-size: 32.3px;
   font-weight: 700;
 
-  margin-bottom: 10px;
+  max-width: 600px;
+  width: 100%;
+
+  h3 {
+    font-size: 20px;
+    font-weight: 400;
+  }
 `;
 
 const SocialLinks = styled.ul`
   display: flex;
-  flex-direction: column;
-  gap: 10px;
+  gap: 20px;
   color: ${theme.colors.font1};
 `;
 
 const Item = styled.li`
-  font-size: 15px;
-  font-weight: 600;
+  font-size: 20px;
   width: max-content;
 
   position: relative;
@@ -108,18 +67,25 @@ const Contact = styled.div`
   justify-content: space-between;
 `;
 
-const ContactMe = styled.div``;
+const ContactMe = styled.div`
+  margin-bottom: 90px;
+`;
 
 const Email = styled.div`
-  font-size: 20px;
+  font-size: 32.3px;
   font-weight: 700;
+
+  max-width: 600px;
+  width: 100%;
+
+  h3 {
+    font-size: 20px;
+    font-weight: 400;
+  }
 `;
 
 export const S = {
   Contacts,
-  MessageBlock,
-  Form,
-  Field,
   PhoneNumber,
   SocialLinks,
   Item,
--- src/layout/sections/contacts/Contacts.tsx
+++ src/layout/sections/contacts/Contacts.tsx
@@ -1,85 +1,39 @@
-import { ElementRef, FC, useRef } from "react";
+import { FC } from "react";
 import { S } from "./Contacts.styles";
 import { StyledContainer } from "../../../components/Container";
 import { FlexWrapper } from "../../../components/FlexWrapper";
-import { StyledBtn } from "../../../components/Button";
 import { SectionTitle } from "../../../components/SectionTitle";
-import emailjs from "@emailjs/browser";
 
 interface ContactsProps {}
 
 const Contacts: FC<ContactsProps> = () => {
-  const form = useRef<ElementRef<"form">>(null);
-
-  const sendEmail = (e: any) => {
-    e.preventDefault();
-
-    if (!form.current) return;
-
-    console.log(form.current);
-
-    emailjs
-      .sendForm("service_xe3wkqf", "template_s8yidcg", form.current, {
-        publicKey: "t6hUi_sF5Vqg431PX",
-      })
-      .then(
-        () => {
-          console.log("SUCCESS!");
-          e.target.reset();
-        },
-        (error) => {
-          console.log("FAILED...", error.text);
-        }
-      );
-  };
-
   return (
     <S.Contacts id="contacts">
       <StyledContainer>
         <SectionTitle>Contact Me</SectionTitle>
-        <FlexWrapper justify="space-between" gap="50px" wrap="wrap">
-          <S.MessageBlock>
-            <S.Form ref={form} onSubmit={sendEmail}>
-              <S.Field
-                type="text"
-                placeholder="Name"
-                name="user_name"
-                required
-              />
-              <S.Field
-                type="email"
-                placeholder="Email"
-                name="user_email"
-                required
-              />
-              <S.Field
-                as={"textarea"}
-                placeholder="Message"
-                name="message"
-                required
-              />
-              <StyledBtn type="submit">Send</StyledBtn>
-            </S.Form>
-          </S.MessageBlock>
-          <S.Contact>
-            <S.ContactMe>
-              <S.PhoneNumber>+7 707 835-52-33</S.PhoneNumber>
-              <S.Email>slehder5657@gmail.com</S.Email>
-            </S.ContactMe>
-
-            <S.SocialLinks>
-              <S.Item>
-                <S.Link>Instagram</S.Link>
-              </S.Item>
-              <S.Item>
-                <S.Link>Linkedin</S.Link>
-              </S.Item>
-              <S.Item>
-                <S.Link>hh</S.Link>
-              </S.Item>
-            </S.SocialLinks>
-          </S.Contact>
-        </FlexWrapper>
+        <S.ContactMe>
+          <FlexWrapper justify="space-between" gap="50px" wrap="wrap">
+            <S.Email>
+              <h3>email</h3>
+              <div>slehder5657@gmail.com</div>
+            </S.Email>
+            <S.PhoneNumber>
+              <h3>mobile</h3>
+              <div>+7 707 835-52-33</div>
+            </S.PhoneNumber>
+          </FlexWrapper>
+        </S.ContactMe>
+        <S.SocialLinks>
+          <S.Item>
+            <S.Link>Instagram</S.Link>
+          </S.Item>
+          <S.Item>
+            <S.Link>Linkedin</S.Link>
+          </S.Item>
+          <S.Item>
+            <S.Link>HH</S.Link>
+          </S.Item>
+        </S.SocialLinks>
       </StyledContainer>
     </S.Contacts>
   );
